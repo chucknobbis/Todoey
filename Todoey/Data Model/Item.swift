0adb5673bdd4673e23b9f7c1252b566dcb92be72
Kris Flajs <Kris@miran-flajs.t-2.net>
2017-12-29 10:56:22 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kris Flajs on 29/12/2017.
+//  Copyright © 2017 Kris Flajs. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
